6a5a1d34c4a3d682f1848e9493b323761af8298b
Muhammad risdham <muhammadrisdham@gmail.com>
2019-10-02 16:34:41 +0800

    Add files via upload

---
+++ MQTT-Broker/pubs.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("Topic: %s\n", msg.Topic())
+	fmt.Printf("Message: %s\n", msg.Payload())
+}
+
+func main() {
+	// Config
+	opts := MQTT.NewClientOptions().AddBroker("localhost:1883")
+	opts.SetClientID("publisher")
+	opts.SetDefaultPublishHandler(f)
+
+	// Connect ke broker
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	msg := fmt.Sprintf("Tugas 4 Network Programming")
+	token := c.Publish("TUGAS 4", 0, false, msg)
+	token.Wait()
+
+	c.Disconnect(250)
+}
